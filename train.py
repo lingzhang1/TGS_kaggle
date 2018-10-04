657a873c0bb201072e13aef2a99c11d2fa84e11d
Ling Zhang <dearlingzhang@gmail.com>
2018-10-04 12:52:50 -0400

    test

---
+++ train.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import random
+
+import pandas as pd
+import numpy as np
+# import matplotlib.pyplot as plt
+# plt.style.use('seaborn-white')
+# import seaborn as sns
+# sns.set_style("white")
+
+import cv2
+from sklearn.model_selection import StratifiedKFold
+
+from tqdm import tqdm_notebook #, tnrange
+#from itertools import chain
+from skimage.io import imread, imshow #, concatenate_images
+from skimage.transform import resize
+from skimage.morphology import label
+
+from keras.models import Model, load_model, save_model
+from keras.layers import Input,Dropout,BatchNormalization,Activation,Add
+from keras.layers.core import Lambda
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.merge import concatenate
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from keras import backend as K
+from keras import optimizers
+
+import tensorflow as tf
+
+from keras.preprocessing.image import array_to_img, img_to_array, load_img#,save_img
+
+import time
+t_start = time.time()
