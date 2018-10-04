32933bf1d87d54635bb9ae309a3a7f411a964d6b
Ling Zhang <dearlingzhang@gmail.com>
2018-10-04 14:22:57 -0400

    test

--- train.py
+++ train.py
@@ -134,6 +134,7 @@ cv_index = 1
 train_index = train_all[cv_index-1]
 evaluate_index = evaluate_all[cv_index-1]
 
+# ########### 10 ##########
 print(train_index.shape,evaluate_index.shape)
 histall = histcoverage(train_df.coverage_class[train_index].values)
 print(f'train cv{cv_index}, number of each mask class = \n \t{histall}')
@@ -153,3 +154,203 @@ print(f'evaluate cv{cv_index}, number of each mask class = \n \t {histall_test}'
 #             j += 1
 #             if(j>=2):
 #                 break
+
+# ########### 11 ##########
+def BatchActivate(x):
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    return x
+
+def convolution_block(x, filters, size, strides=(1,1), padding='same', activation=True):
+    x = Conv2D(filters, size, strides=strides, padding=padding)(x)
+    if activation == True:
+        x = BatchActivate(x)
+    return x
+
+def residual_block(blockInput, num_filters=16, batch_activate = False):
+    x = BatchActivate(blockInput)
+    x = convolution_block(x, num_filters, (3,3) )
+    x = convolution_block(x, num_filters, (3,3), activation=False)
+    x = Add()([x, blockInput])
+    if batch_activate:
+        x = BatchActivate(x)
+    return x
+
+
+# ########### 12 ##########
+    # Build model
+def build_model(input_layer, start_neurons, DropoutRatio = 0.5):
+    # 101 -> 50
+    conv1 = Conv2D(start_neurons * 1, (3, 3), activation=None, padding="same")(input_layer)
+    conv1 = residual_block(conv1,start_neurons * 1)
+    conv1 = residual_block(conv1,start_neurons * 1, True)
+    pool1 = MaxPooling2D((2, 2))(conv1)
+    pool1 = Dropout(DropoutRatio/2)(pool1)
+
+    # 50 -> 25
+    conv2 = Conv2D(start_neurons * 2, (3, 3), activation=None, padding="same")(pool1)
+    conv2 = residual_block(conv2,start_neurons * 2)
+    conv2 = residual_block(conv2,start_neurons * 2, True)
+    pool2 = MaxPooling2D((2, 2))(conv2)
+    pool2 = Dropout(DropoutRatio)(pool2)
+
+    # 25 -> 12
+    conv3 = Conv2D(start_neurons * 4, (3, 3), activation=None, padding="same")(pool2)
+    conv3 = residual_block(conv3,start_neurons * 4)
+    conv3 = residual_block(conv3,start_neurons * 4, True)
+    pool3 = MaxPooling2D((2, 2))(conv3)
+    pool3 = Dropout(DropoutRatio)(pool3)
+
+    # 12 -> 6
+    conv4 = Conv2D(start_neurons * 8, (3, 3), activation=None, padding="same")(pool3)
+    conv4 = residual_block(conv4,start_neurons * 8)
+    conv4 = residual_block(conv4,start_neurons * 8, True)
+    pool4 = MaxPooling2D((2, 2))(conv4)
+    pool4 = Dropout(DropoutRatio)(pool4)
+
+    # Middle
+    convm = Conv2D(start_neurons * 16, (3, 3), activation=None, padding="same")(pool4)
+    convm = residual_block(convm,start_neurons * 16)
+    convm = residual_block(convm,start_neurons * 16, True)
+
+    # 6 -> 12
+    deconv4 = Conv2DTranspose(start_neurons * 8, (3, 3), strides=(2, 2), padding="same")(convm)
+    uconv4 = concatenate([deconv4, conv4])
+    uconv4 = Dropout(DropoutRatio)(uconv4)
+
+    uconv4 = Conv2D(start_neurons * 8, (3, 3), activation=None, padding="same")(uconv4)
+    uconv4 = residual_block(uconv4,start_neurons * 8)
+    uconv4 = residual_block(uconv4,start_neurons * 8, True)
+
+    # 12 -> 25
+    #deconv3 = Conv2DTranspose(start_neurons * 4, (3, 3), strides=(2, 2), padding="same")(uconv4)
+    deconv3 = Conv2DTranspose(start_neurons * 4, (3, 3), strides=(2, 2), padding="valid")(uconv4)
+    uconv3 = concatenate([deconv3, conv3])
+    uconv3 = Dropout(DropoutRatio)(uconv3)
+
+    uconv3 = Conv2D(start_neurons * 4, (3, 3), activation=None, padding="same")(uconv3)
+    uconv3 = residual_block(uconv3,start_neurons * 4)
+    uconv3 = residual_block(uconv3,start_neurons * 4, True)
+
+    # 25 -> 50
+    deconv2 = Conv2DTranspose(start_neurons * 2, (3, 3), strides=(2, 2), padding="same")(uconv3)
+    uconv2 = concatenate([deconv2, conv2])
+
+    uconv2 = Dropout(DropoutRatio)(uconv2)
+    uconv2 = Conv2D(start_neurons * 2, (3, 3), activation=None, padding="same")(uconv2)
+    uconv2 = residual_block(uconv2,start_neurons * 2)
+    uconv2 = residual_block(uconv2,start_neurons * 2, True)
+
+    # 50 -> 101
+    #deconv1 = Conv2DTranspose(start_neurons * 1, (3, 3), strides=(2, 2), padding="same")(uconv2)
+    deconv1 = Conv2DTranspose(start_neurons * 1, (3, 3), strides=(2, 2), padding="valid")(uconv2)
+    uconv1 = concatenate([deconv1, conv1])
+
+    uconv1 = Dropout(DropoutRatio)(uconv1)
+    uconv1 = Conv2D(start_neurons * 1, (3, 3), activation=None, padding="same")(uconv1)
+    uconv1 = residual_block(uconv1,start_neurons * 1)
+    uconv1 = residual_block(uconv1,start_neurons * 1, True)
+
+    #uconv1 = Dropout(DropoutRatio/2)(uconv1)
+    #output_layer = Conv2D(1, (1,1), padding="same", activation="sigmoid")(uconv1)
+    output_layer_noActi = Conv2D(1, (1,1), padding="same", activation=None)(uconv1)
+    output_layer =  Activation('sigmoid')(output_layer_noActi)
+
+    return output_layer
+
+
+# ########### 13 ##########
+
+def get_iou_vector(A, B):
+    A = np.squeeze(A) # new added
+    B = np.squeeze(B) # new added
+    batch_size = A.shape[0]
+    metric = []
+    for batch in range(batch_size):
+        t, p = A[batch]>0, B[batch]>0
+        if np.count_nonzero(t) == 0 and np.count_nonzero(p) > 0:
+            metric.append(0)
+            continue
+        if np.count_nonzero(t) >= 1 and np.count_nonzero(p) == 0:
+            metric.append(0)
+            continue
+        if np.count_nonzero(t) == 0 and np.count_nonzero(p) == 0:
+            metric.append(1)
+            continue
+
+        intersection = np.logical_and(t, p)
+        union = np.logical_or(t, p)
+        iou = (np.sum(intersection > 0)  )/ (np.sum(union > 0) )
+        thresholds = np.arange(0.5, 1, 0.05)
+        s = []
+        for thresh in thresholds:
+            s.append(iou > thresh)
+        metric.append(np.mean(s))
+
+    return np.mean(metric)
+
+def my_iou_metric(label, pred):
+    return tf.py_func(get_iou_vector, [label, pred>0.5], tf.float64)
+
+def my_iou_metric_2(label, pred):
+    return tf.py_func(get_iou_vector, [label, pred >0], tf.float64)
+def build_complie_model(lr = 0.01):
+    input_layer = Input((img_size_target, img_size_target, 1))
+    output_layer = build_model(input_layer, 16,0.5)
+
+    model1 = Model(input_layer, output_layer)
+
+    c = optimizers.adam(lr = lr)
+    model1.compile(loss="binary_crossentropy", optimizer=c, metrics=[my_iou_metric])
+    return model1
+# def plot_history(history,metric_name):
+#     fig, (ax_loss, ax_score) = plt.subplots(1, 2, figsize=(15,5))
+#     ax_loss.plot(history.epoch, history.history["loss"], label="Train loss")
+#     ax_loss.plot(history.epoch, history.history["val_loss"], label="Validation loss")
+#     ax_loss.legend()
+#     ax_score.plot(history.epoch, history.history[metric_name], label="Train score")
+#     ax_score.plot(history.epoch, history.history["val_" + metric_name], label="Validation score")
+#     ax_score.legend()
+
+def predict_result(model,x_test,img_size_target): # predict both orginal and reflect x
+    x_test_reflect =  np.array([np.fliplr(x) for x in x_test])
+    preds_test = model.predict(x_test).reshape(-1, img_size_target, img_size_target)
+    preds_test2_refect = model.predict(x_test_reflect).reshape(-1, img_size_target, img_size_target)
+    preds_test += np.array([ np.fliplr(x) for x in preds_test2_refect] )
+    return preds_test/2
+# training
+ious = [0] * cv_total
+for cv_index in range(cv_total):
+    basic_name = f'Unet_resnet_v{version}_cv{cv_index+1}'
+    print('############################################\n', basic_name)
+    save_model_name = basic_name + '.model'
+
+    x_train, y_train, x_valid, y_valid =  get_cv_data(cv_index+1)
+
+    #Data augmentation
+    x_train = np.append(x_train, [np.fliplr(x) for x in x_train], axis=0)
+    y_train = np.append(y_train, [np.fliplr(x) for x in y_train], axis=0)
+
+    model = build_complie_model(lr = 0.01)
+
+    model_checkpoint = ModelCheckpoint(save_model_name,monitor='val_my_iou_metric',
+                                   mode = 'max', save_best_only=True, verbose=1)
+    reduce_lr = ReduceLROnPlateau(monitor='val_my_iou_metric', mode = 'max',
+                                  factor=0.5, patience=3, min_lr=0.0001, verbose=1)
+
+    epochs = 20 #small number for demonstration
+    batch_size = 32
+    history = model.fit(x_train, y_train,
+                        validation_data=[x_valid, y_valid],
+                        epochs=epochs,
+                        batch_size=batch_size,
+                        callbacks=[ model_checkpoint,reduce_lr],
+                        verbose=2)
+    plot_history(history,'my_iou_metric')
+
+    model.load_weights(save_model_name)
+
+    preds_valid = predict_result(model,x_valid,img_size_target)
+    ious[cv_index] = get_iou_vector(y_valid, (preds_valid > 0.5))
+
+#model1.summary()
